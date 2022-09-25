4dc4f2961416d8a75d7a6eb006e4cf002aeda3e5
Matthew Nazari <matthewanazari@gmail.com>
2022-09-25 19:01:39 -0400

    add mobile support, add content

---
+++ script.js
@@ -0,0 +1,65 @@
+const switchSection = sectionId => {
+    document.querySelectorAll('.section')
+            .forEach(e => e.classList.add('hide'))
+    document.querySelector('#' + sectionId).classList.remove('hide')
+}
+
+switchSection('about')
+
+const hideCourses = semesterId =>
+    document.querySelectorAll('.' + semesterId)
+            .forEach(e => e.classList.toggle('hide'))
+
+hideCourses('s22')
+hideCourses('f21')
+hideCourses('s21')
+hideCourses('f20')
+
+names = [
+    'Matthew Nazari',
+    'ܡܬܝ ܢܙܪܝ̣',
+    'متیو نظری',
+    'Մեթյու Նազարյան',
+]
+
+fontSizes = [2.25, 5, 3.5, 2.25]
+
+const nameElem = document.querySelector('#side #name')
+
+const deleteName = async _ => {
+    let intervalId = null
+    intervalId = setInterval(_ => {
+        if (nameElem.innerText) {
+            nameElem.innerText = nameElem.innerText.slice(0, -1)
+        } else {
+            clearInterval(intervalId)
+        }
+    }, 50)
+}
+
+const typeName = async name => {
+    let intervalId = null
+    intervalId = setInterval(_ => {
+        if (name) {
+            let toAppend = ''
+            while (toAppend === ' ' || toAppend === '') {
+                toAppend = toAppend + name.charAt(0)
+                name = name.slice(1)
+            }
+            nameElem.innerText = nameElem.innerText + toAppend
+        } else {
+            clearInterval(intervalId)
+        }
+    }, 50)
+}
+
+let iteration = 0
+setInterval(_ => {
+    iteration = (iteration + 1) % names.length
+    deleteName()
+    setTimeout(_ => {
+            nameElem.style.fontSize = fontSizes[iteration] + 'em'
+            typeName(names[iteration])
+        }
+    , 1000)
+}, 7000)
